e4d4856acc7bb6c61961632742cf97f6ce640954
lkraus5 <49344309+lkraus5@users.noreply.github.com>
2024-10-14 10:39:01 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import { generateClient } from "aws-amplify/data";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
 
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
+      {/* <button onClick={createTodo}>+ new</button> */}
+      {/* <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
         ))}
-      </ul>
+      </ul> */}
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
