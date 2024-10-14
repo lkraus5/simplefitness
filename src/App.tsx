d420e50b5fcc6552732a06d05d0a07226324d2e8
lkraus5 <49344309+lkraus5@users.noreply.github.com>
2024-10-14 10:43:48 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
 // import { generateClient } from "aws-amplify/data";
 
 // const client = generateClient<Schema>();
@@ -10,20 +10,20 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      {/* <h1>My todos</h1> */}
       {/* <button onClick={createTodo}>+ new</button> */}
       {/* <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
         ))}
       </ul> */}
-      <div>
+      {/* <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
