e2e3a44b1368f12a87e1743a4c413c3f5b53973a
lkraus5 <49344309+lkraus5@users.noreply.github.com>
2024-10-14 17:28:40 -0500

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -6,128 +6,124 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
-const schema = a.schema({
-  Exercise: a.model({
-    name: a.string(),
-    targetMuscles: a.string().array(),
-    description: a.string()
-  }).authorization((allow) => [allow.authenticated()]),
-  UserExercise: a.model({
-    name: a.string(),
-    targetMuscles: a.string().array(),
-    description: a.string(),
-    personalNotes: a.string(),
-    exerciseId: a.id(),
-    workoutId: a.id(),
-    workout: a.belongsTo("Workout", "workoutId"),
-  }).secondaryIndexes((index) => [index("workoutId").name("UserExerciseByWorkoutId")]).authorization((allow) => [allow.authenticated()]),
-  Workout: a.model({
-    name: a.string(),
-    description: a.string(),
-    workoutPeriodId: a.id(),
-    workoutPeriod: a.belongsTo("WorkoutPeriod", "workoutPeriodId"),
-    exercises: a.hasMany("UserExercise", "workoutId"),
-    sessions: a.hasMany("SessionsByWorkoutId", "workoutId")
-  }).secondaryIndexes((index) => [index("workoutPeriodId").name("WorkoutByWorkoutPeriodId")]).authorization((allow) => [allow.authenticated()]),
-  Session: a.model({
-    sessionWorkoutId: a.id(),
-    sessionWorkout: a.belongsTo("Workout", "sessionWorkoutId"),
-    fueledFeeling: a.string(),
-    muscleFeeling: a.string(),
-    sets: a.hasMany("setsBySessionId", "setSessionId")
-  }).secondaryIndexes((index) => [index("sessionWorkoutId").name("SessionsByWorkoutId")]).authorization((allow) => [allow.authenticated()]),
-  Set: a.model({
-    setSessionId: a.id(),
-    setSession: a.belongsTo("Session", "setSessionId"),    
-    reps: a.string(),
-    weight: a.string(),
-    rangeOfMotion: a.string(),
-    feeling: a.string(),
-    effort: a.string(),
-  }).secondaryIndexes((index) => [index("setSessionId").name("setsBySessionId")]).authorization((allow) => [allow.authenticated()]),
-  WorkoutPeriod: a.model({
-    workouts: a.hasMany("WorkoutByWorkoutPeriodId", "workoutPeriodId"),
-    description: a.string(),
-    muscleFeeling: a.string(),
-    mesoPeroidId: a.id(),
-    mesoPeriod: a.belongsTo("MesoPeriod", "mesoPeroidId"),
-  }).secondaryIndexes((index) => [index("mesoPeroidId").name("WorkoutPeriodByMesoId")]).authorization((allow) => [allow.authenticated()]),
-  MesoPeriod: a.model({
-    workoutPeriods: a.hasMany("WorkoutPeriodByMesoId", "mesoPeroidId"),
-    description: a.string(),
-    muscleFeeling: a.string(),
-  }).authorization((allow) => [allow.authenticated()]),
-});
+// const schema = a.schema({
+//   Exercise: a.model({
+//     name: a.string(),
+//     targetMuscles: a.string().array(),
+//     description: a.string()
+//   }).authorization((allow) => [allow.authenticated()]),
+//   UserExercise: a.model({
+//     name: a.string(),
+//     targetMuscles: a.string().array(),
+//     description: a.string(),
+//     personalNotes: a.string(),
+//     exerciseId: a.id(),
+//     workoutId: a.id(),
+//     workout: a.belongsTo("Workout", "workoutId"),
+//   }).secondaryIndexes((index) => [index("workoutId").name("UserExerciseByWorkoutId")]).authorization((allow) => [allow.authenticated()]),
+//   Workout: a.model({
+//     name: a.string(),
+//     description: a.string(),
+//     workoutPeriodId: a.id(),
+//     workoutPeriod: a.belongsTo("WorkoutPeriod", "workoutPeriodId"),
+//     exercises: a.hasMany("UserExercise", "workoutId"),
+//     sessions: a.hasMany("SessionsByWorkoutId", "workoutId")
+//   }).secondaryIndexes((index) => [index("workoutPeriodId").name("WorkoutByWorkoutPeriodId")]).authorization((allow) => [allow.authenticated()]),
+//   Session: a.model({
+//     sessionWorkoutId: a.id(),
+//     sessionWorkout: a.belongsTo("Workout", "sessionWorkoutId"),
+//     fueledFeeling: a.string(),
+//     muscleFeeling: a.string(),
+//     sets: a.hasMany("setsBySessionId", "setSessionId")
+//   }).secondaryIndexes((index) => [index("sessionWorkoutId").name("SessionsByWorkoutId")]).authorization((allow) => [allow.authenticated()]),
+//   Set: a.model({
+//     setSessionId: a.id(),
+//     setSession: a.belongsTo("Session", "setSessionId"),    
+//     reps: a.string(),
+//     weight: a.string(),
+//     rangeOfMotion: a.string(),
+//     feeling: a.string(),
+//     effort: a.string(),
+//   }).secondaryIndexes((index) => [index("setSessionId").name("setsBySessionId")]).authorization((allow) => [allow.authenticated()]),
+//   WorkoutPeriod: a.model({
+//     workouts: a.hasMany("WorkoutByWorkoutPeriodId", "workoutPeriodId"),
+//     description: a.string(),
+//     muscleFeeling: a.string(),
+//     mesoPeroidId: a.id(),
+//     mesoPeriod: a.belongsTo("MesoPeriod", "mesoPeroidId"),
+//   }).secondaryIndexes((index) => [index("mesoPeroidId").name("WorkoutPeriodByMesoId")]).authorization((allow) => [allow.authenticated()]),
+//   MesoPeriod: a.model({
+//     workoutPeriods: a.hasMany("WorkoutPeriodByMesoId", "mesoPeroidId"),
+//     description: a.string(),
+//     muscleFeeling: a.string(),
+//   }).authorization((allow) => [allow.authenticated()]),
+// });
 
-// const schema = `
-// type Exercise @model  @auth(rules: [ {allow: private, provider: userPools, operations: [read]}, { allow: private, provider: iam}])  {
-//   id: ID!
-//   name: String!
-//   targetedMuscles: [String]
-//   description: String
-// }
+const schema = `
+type Exercise @model  @auth(rules: [ {allow: private, provider: userPools, operations: [read]}, { allow: private, provider: iam}])  {
+  id: ID!
+  name: String!
+  targetedMuscles: [String]
+  description: String
+}
 
-// type Workout @model @auth(rules: [ {allow: owner}, { allow: private, provider: iam}]) {
-//   id: ID!
-//   name: String!
-//   exerciseids: [ID]
-//   exercises: [Exercise] @hasMany(fields: ["exerciseids"])
-//   description: String
-//   sessions: [Session] @hasMany(indexName:"sessionByWorkout", fields:["id"])
-// }
+type Workout @model @auth(rules: [ {allow: owner}, { allow: private, provider: iam}]) {
+  id: ID!
+  name: String!
+  exerciseids: [ID]
+  exercises: [Exercise] @hasMany(fields: ["exerciseids"])
+  description: String
+  sessions: [Session] @hasMany(indexName:"sessionByWorkout", fields:["id"])
+}
 
-// type Session @model @auth(rules: [ {allow: owner}, { allow: private, provider: iam}]) {
-//   id: ID!
-//   workout: ID @index(name: "sessionByWorkout", sortKeyFields: ["createdAt"])
-//   fueledFeeling: String
-//   muscleFeeling: String
-//   sets: [Set] @hasMany(indexName:"setBySessionAndExercise", fields:["id"])
-//   createdAt: String!
-//   updatedAt: String!
-// }
+type Session @model @auth(rules: [ {allow: owner}, { allow: private, provider: iam}]) {
+  id: ID!
+  workout: ID @index(name: "sessionByWorkout", sortKeyFields: ["createdAt"])
+  fueledFeeling: String
+  muscleFeeling: String
+  sets: [Set] @hasMany(indexName:"setBySessionAndExercise", fields:["id"])
+  createdAt: String!
+  updatedAt: String!
+}
 
-// type Set @model @auth(rules: [ {allow: owner}, { allow: private, provider: iam}]) {
-//   id: ID!
-//   session: ID @index(name: "setBySessionAndExercise", sortKeyFields: ["exerciseid", "createdAt"]) @index(name: "setBySession", sortKeyFields: ["createdAt"])
-//   exerciseid: ID @index(name: "setByExerciseAndSession", sortKeyFields: ["session", "createdAt"]) @index(name: "setByExercise", sortKeyFields: ["createdAt"])
-//   exercise: Exercise @hasOne(fields: ["exerciseid"])
-//   reps: String
-//   weight: String
-//   repsInReserve: String
-//   rangeOfMotion: String
-//   feeling: String
-//   effort: String
-//   createdAt: String!
-//   updatedAt: String!
-// }
+type Set @model @auth(rules: [ {allow: owner}, { allow: private, provider: iam}]) {
+  id: ID!
+  session: ID @index(name: "setBySessionAndExercise", sortKeyFields: ["exerciseid", "createdAt"]) @index(name: "setBySession", sortKeyFields: ["createdAt"])
+  exerciseid: ID @index(name: "setByExerciseAndSession", sortKeyFields: ["session", "createdAt"]) @index(name: "setByExercise", sortKeyFields: ["createdAt"])
+  exercise: Exercise @hasOne(fields: ["exerciseid"])
+  reps: String
+  weight: String
+  repsInReserve: String
+  rangeOfMotion: String
+  feeling: String
+  effort: String
+  createdAt: String!
+  updatedAt: String!
+}
 
-// type WorkoutPeriod @model @auth(rules: [ {allow: owner}, { allow: private, provider: iam}]) {
-//   id: ID!
-//   name: String!
-//   workoutids: [ID]
-//   workouts: [Workout] @hasMany(fields: ["workoutids"])
-//   description: String
-// }
+type WorkoutPeriod @model @auth(rules: [ {allow: owner}, { allow: private, provider: iam}]) {
+  id: ID!
+  name: String!
+  workoutids: [ID]
+  workouts: [Workout] @hasMany(fields: ["workoutids"])
+  description: String
+}
 
-// type MesoPeriod @model @auth(rules: [ {allow: owner}, { allow: private, provider: iam}]) {
-//   id: ID!
-//   name: String!
-//   workoutPeriodIds: [ID]
-//   workoutPeriods: [WorkoutPeriod] @hasMany(fields: ["workoutPeriodIds"])
-//   description: String
-// }`
+type MesoPeriod @model @auth(rules: [ {allow: owner}, { allow: private, provider: iam}]) {
+  id: ID!
+  name: String!
+  workoutPeriodIds: [ID]
+  workoutPeriods: [WorkoutPeriod] @hasMany(fields: ["workoutPeriodIds"])
+  description: String
+}`
 
 
-export type Schema = ClientSchema<typeof schema>;
+// export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: "userPool",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
   },
 });
 
