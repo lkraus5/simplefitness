9448a3acf316bd511adda74f3beab92b19aac5f8
lkraus5 <49344309+lkraus5@users.noreply.github.com>
2024-10-15 11:39:17 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
 import { Authenticator } from '@aws-amplify/ui-react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import {
   Button
 } from "reactstrap";
 // import { generateClient } from "aws-amplify/data";
-// import ExerciseCreateForm from "../ui-components/ExerciseCreateForm"
-import SetupPage from './pages/Setup';
+import ExerciseCreateForm from "../ui-components/ExerciseCreateForm"
+// import SetupPage from './pages/Setup';
 // const client = generateClient<Schema>();
 
 export default function App() {
-  // const [workouts, setWorkouts] = useState<Array<Schema["Workout"]["type"]>>([]);
 
-
+  
   return (
     <Authenticator loginMechanisms={['username']}>
       {({ signOut }) => <>
         <Router>
           <nav className='mainNavbar'>
-            {/* <img src="/mooserun2Logo-smaller-transparent.png" alt="image" style={{height: "60px", width: "60px", top: "14px", left: "10px"}} /> */}
-
-
             <Button className='signOutButton' onClick={signOut}>Sign out</Button>
           </nav>
           <main className='appMain'>
             <Routes>
-              <Route path="/" element={<SetupPage />} />
+              <Route path="/" element={<ExerciseCreateForm />} />
             </Routes>
           </main>
         </Router>
