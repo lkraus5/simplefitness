1be92a06a1ac9634b3831e7311adf1df69b1e6c9
lkraus5 <49344309+lkraus5@users.noreply.github.com>
2024-10-16 15:31:34 -0500

    Update ExercisesList.tsx

--- src/components/ExercisesList.tsx
+++ src/components/ExercisesList.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { useEffect, useState } from 'react';
 // import { Link } from "react-router-dom";
 import './ExercisesList.css'
 import { generateClient } from "aws-amplify/api";
 import { listExercises } from "../../ui-components/graphql/queries";
-// import {  ListGroup, ListGroupItem } from "reactstrap";
+import {  ListGroup, ListGroupItem } from "reactstrap";
 
 
 // async getInstanceTypes(){
@@ -34,21 +35,23 @@ const ListExercises = async () => {
     }
     // console.log(Items)
     setExercises(Items)
-    // const exerciseList = Items.map((exercise: Object) => {
-    //     console.log({exercise})
-    //     return(<>
-    //         <ListGroupItem>
-    //             Test
-    //         </ListGroupItem>
-    //     </>)
-    // })
+    const exerciseList = Items.map((exercise: Object) => {
+        return(
+            <React.Fragment>
+                <ListGroupItem>
+                    {exercise}
+                </ListGroupItem>
+            </React.Fragment>
+        )
+    })
+
     // return(<>
     // <ListGroup>
     //     {exerciseList}
     // </ListGroup>
     // </>)
     // setExercises(exerciseList)
-    // return(exerciseList)
+    setExercises(exerciseList)
 }
 
 
@@ -57,7 +60,9 @@ export default function ExercisesList() {
     // const exerciseList = GetExercises()
     // GetExercises()
     return(<>
-
+    <ListGroup>
+        {exercises}
+    </ListGroup>
     </>)
 
 }
